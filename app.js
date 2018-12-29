dcb85b832ad24d0c8839668afe02a3a4d9736f0e
Nazar94 <nazar.masliy94@gmail.com>
2018-12-29 23:24:20 +0200

    added user sign up, installed bcryptjs

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose')
 
 const productRoutes = require('./api/routes/products')
 const orderRoutes = require('./api/routes/orders')
+const userRoutes = require('./api/routes/users')
 const uri = 'mongodb://localhost:27017/shop2'
 //Use morgan:
 app.use(morgan('dev'))
@@ -41,7 +42,7 @@ app.use((req, res, next)=> {
 
 app.use('/products', productRoutes)
 app.use('/orders', orderRoutes)
-
+app.use('/user', userRoutes)
 //Handling errors
 app.use((req, res, next) => {
   const error = new Error('Not found')
